45da0d5fb3d251632190b40a010211955fae74c3
zhouminghao <467914486@qq.com>
2024-08-15 02:12:46 +0800

    canal项目搭建 首次提交

---
+++ src/main/java/com/zmh/canal/StartApplication.java
@@ -0,0 +1,13 @@
+package com.zmh.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class,args);
+    }
+}
---
+++ src/main/java/com/zmh/canal/common/config/CanalClient.java
@@ -0,0 +1,12 @@
+package com.zmh.canal.common.config;
+
+/**
+ * @Description: CanalClient
+ * @author: zhou ming hao
+ * @date: 2024年08月15日 1:49
+ */
+
+public class CanalClient {
+
+
+}
